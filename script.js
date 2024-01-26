4a70c6990269c614bd08e9862e2d19354d14f8f8
Anh Vu <anhvu11102003@gmail.com>
2024-01-26 16:38:10 +0700

    quen commit hoi nhieu =))

--- script.js
+++ script.js
@@ -11,3 +11,25 @@ window.onload = function () { document.body.classList.remove('is-preload'); }
 		function openTV() {
 			window.open('https://anhzuuu.github.io/happynewyear/', 'Popup', 'width=600,height=400');
 		}
+
+        function getDeviceSize() {
+            let height = screen.height;
+            let width = screen.width;
+            document.getElementById("screen").innerHTML =  width + "x"+ height + "px";
+        }
+        function openPopup2() {
+			document.getElementById("screen").style.display = "block";
+            document.getElementById("gl-bt").style.display = "flex";
+            document.getElementById("gl-bt-wr").style.display = "table";
+            
+            let height = screen.height;
+            let width = screen.width;
+            document.getElementById("screen").innerHTML = "Your device is " + width + "x"+ height + "px";
+		}
+
+		function closePopup2() {
+			document.getElementById("screen").style.display = "none";
+            document.getElementById("gl-bt").style.display = "none";
+            document.getElementById("gl-bt-wr").style.display = "none";
+            
+		}
