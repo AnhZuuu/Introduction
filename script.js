0ef6825ecfce3bd7260f050ca3ab86237fdab74b
Anh Vu <anhvu11102003@gmail.com>
2024-01-30 16:57:37 +0700

    4:57 30/1

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+//glitching
+let anims = [...document.querySelectorAll("[anim]")];
+console.log(anims);
+let click = (el, cb) => el.addEventListener("click", cb);
+let toggle = (el) => el.classList.toggle("toggled");
+let clickTog = (el) => click(el, () => toggle(el));
+anims.map(clickTog);
+
+
+//Popup management
 window.onload = function () { document.body.classList.remove('is-preload'); }
 		window.ontouchmove = function () { return false; }
 		window.onorientationchange = function () { document.body.scrollTop = 0; }
@@ -12,15 +22,18 @@ window.onload = function () { document.body.classList.remove('is-preload'); }
 			window.open('https://anhzuuu.github.io/happynewyear/', 'Popup', 'width=600,height=400');
 		}
 
-        function getDeviceSize() {
-            let height = screen.height;
-            let width = screen.width;
-            document.getElementById("screen").innerHTML =  width + "x"+ height + "px";
-        }
+        // function getDeviceSize() {
+        //     let height = screen.height;
+        //     let width = screen.width;
+        //     document.getElementById("screen").innerHTML =  width + "x"+ height + "px";
+        // }
         function openPopup2() {
 			document.getElementById("screen").style.display = "block";
             document.getElementById("gl-bt").style.display = "flex";
             document.getElementById("gl-bt-wr").style.display = "table";
+
+			// document.getElementById("screen").style.display = "block";
+			// document.getElementById("glitch-bt").style.display = "block";
             
             let height = screen.height;
             let width = screen.width;
